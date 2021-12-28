b19be1fb7c9f3ad52f678baeaa5a13d5c23eeb83
fearm3 <ibraaa1628@gmail.com>
2021-12-28 15:10:52 +0300

    add commit

--- src/Submenu.js
+++ src/Submenu.js
@@ -4,8 +4,8 @@ import { useGlobalContext } from "./context";
 const Submenu = () => {
   const {
     isSubmenuOpen,
-    location,
     page: { page, links },
+    location,
   } = useGlobalContext();
   const container = useRef(null);
   const [columns, setColumns] = useState("col-2");
@@ -15,31 +15,33 @@ const Submenu = () => {
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-
+    console.log(links);
     if (links.length === 3) {
       setColumns("col-3");
     }
-    if (links.length > 4) {
+    if (links.length > 3) {
       setColumns("col-4");
     }
-  }, [location, links]);
+  }, [page, location, links]);
   return (
     <aside
-      className={`${isSubmenuOpen ? "submenu show " : "submenu"}`}
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
       ref={container}
     >
-      <h4>{page}</h4>
-      <div className={`submenu-center ${columns}`}>
-        {links.map((link, index) => {
-          const { label, icon, url } = link;
-          return (
-            <a key={index} href={url}>
-              {icon}
-              {label}
-            </a>
-          );
-        })}
-      </div>
+      <section>
+        <h4>{page}</h4>
+        <div className={`submenu-center ${columns}`}>
+          {links.map((link, index) => {
+            const { url, icon, label } = link;
+            return (
+              <a key={index} href={url}>
+                {icon}
+                {label}
+              </a>
+            );
+          })}
+        </div>
+      </section>
     </aside>
   );
 };
